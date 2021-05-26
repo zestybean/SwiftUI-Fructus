6ac21287c3802fc36c65fea6303506cdb1945018
Beandip <brando.lugo@gmail.com>
2021-05-25 20:41:39 -0500

    Initial Commit

---
+++ Shared/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
