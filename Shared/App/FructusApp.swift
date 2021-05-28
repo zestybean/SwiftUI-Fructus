4be804b40c50c6801c0874730639c11953bd5ca2
zestybean <brando.lugo@gmail.com>
2021-05-27 19:50:57 -0500

    Implemented the app storage variable to move from the onboarding screen to the content view

--- Shared/App/FructusApp.swift
+++ Shared/App/FructusApp.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
-@main
+@main //MARK: - New to SwiftUI
 struct FructusApp: App {
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                ContentView()
+            }
         }
     }
 }
