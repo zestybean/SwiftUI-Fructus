b305be7258bff90370dafcf12d3188394dba1648
Beandip <brando.lugo@gmail.com>
2021-05-30 20:57:52 -0500

    Started the settings page

--- Shared/App/ContentView.swift
+++ Shared/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     //MARK: - PROPERTIES
     var fruits: [Fruit] = fruitsData
     
+    @State private var isShowingSettings: Bool = false
+    
     //MARK: - BODY
     var body: some View {
         NavigationView{
@@ -25,6 +27,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(trailing: Button(action: {
+                isShowingSettings = true
+            }, label: {
+                Image(systemName: "slider.horizontal.3")
+            }).sheet(isPresented: $isShowingSettings) {
+                SettingsView()
+            })//: BUTTON
         } //: NAVIGATION
     }
 }
---
+++ Shared/App/SettingsView.swift
@@ -0,0 +1,59 @@
+//
+//  SettingsView.swift
+//  Fructus (iOS)
+//
+//  Created by Brando Lugo on 5/30/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    //MARK: - PROPERTIES
+    @Environment(\.presentationMode) var presentationMode
+    
+    //MARK: - BODY
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing: 20) {
+                    //MARK: - SECTION 1
+                    GroupBox(label: SettingsLabelView(labelText: "Fructus", labelImage: "info.circle"), content: {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10, content: {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80, alignment: .center)
+                                .cornerRadius(9)
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including patassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        })
+                    })
+                    //MARK: - SECTION 2
+                    
+                    
+                    //MARK: - SECTION 3
+                    
+                }//: VSTACK
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(trailing: Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark")
+                }))
+                .padding()
+            }//: SCROLL
+        }//: NAVIGATION
+        
+    }
+}
+
+
+//MARK: - PREVIEW
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 11 Pro")
+    }
+}
